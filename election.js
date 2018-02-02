8168990fb208b3c4bfee3f6ce70338b6ffcfedf0
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-02 14:07:58 -0500

    get asynchronous buttons working

--- election.js
+++ election.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
       submitter.innerText = "Vote for " + candi.name
 
       var invisible = document.createElement('input');
-      invisible.setAttribute('hidden', 'true');
+      invisible.setAttribute('type', 'hidden');
       invisible.setAttribute('name', 'name');
       invisible.value = candi.name;
       form.append(invisible);
@@ -29,6 +29,17 @@ document.addEventListener("DOMContentLoaded", function() {
       litem.innerHTML = candi.name + ' Votes: ' + candi.votes
       litem.append(form);
       candidate.append(litem)
+
+      form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: "https://bb-election-api.herokuapp.com/vote",
+          method: "POST",
+          data: {
+            'name': this.querySelector('input[type=hidden]').value
+          }
+        });
+      });
 
     }
   });
